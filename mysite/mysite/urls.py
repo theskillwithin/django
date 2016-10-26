8020ae0c61b41ca328d0567939f2dd9482e05c9d
Austin Peterson <austinair@Austins-MacBook-Air.local>
2016-10-26 17:59:56 -0600

    hello world

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from mysite import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
