cca0ad954e59bd831e97e84ad2e493383ea037e0
Austin Peterson <austinair@Austins-MacBook-Air.local>
2016-10-26 17:53:07 -0600

    sp

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
